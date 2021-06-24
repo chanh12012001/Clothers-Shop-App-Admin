e368761bdebd9330f8e66e8808136184c05413fd
Ho Quang <19520875@gm.uit.edu.vn>
2021-06-25 00:48:36 +0700

    Add main ui and login

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import { Switch , Route , Redirect} from "react-router-dom"
+import Login from './Pages/Login';
+import Dashboard from './Pages/Dashboard';
+import firebase from './firebase';
+import Authenticated from './Components/Authenticated';
+function App() {
+  return (
+    <Switch>
+        <Route exact path="/">
+          <Authenticated>
+            <Dashboard/>
+          </Authenticated>
+        </Route>
+        <Route exact path="/login" >
+          <Authenticated nonAuthenticated={true}>
+              <Login/>
+          </Authenticated>
+        </Route>
+        <Route path="*" render={()=>"404 Not Found"}/>
+    </Switch>
+  );
+}
+
+export default App;
